85a609df7460dd7e331317f927fb3ecf08c3edf1
razmathegreat <flatley.art@gmail.com>
2016-11-27 15:13:29 -0600

    Added alternating pattern to our algorithm folder

---
+++ IntegerCounter/IntegerCounter/Algorithms/alternating.h
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#pragma once
+/*
+alternating used to calculate the following pattern:
+{X,Y,X,Y,X,Y}
+*/
+
+bool alternatingFrontTest(std::vector<double> sequence) {
+	double x = sequence[0];
+	double y = sequence[1];
+	double z = sequence[2];
+	double a = sequence[3];
+	
+	if (x==z && y==a) {
+		return true;
+	}
+	else
+		return false;
+}
+
+bool alternatingBackTest(std::vector<double> sequence) {
+	double x = sequence[sequence.size() - 4];
+	double y = sequence[sequence.size() - 3];
+	double z = sequence[sequence.size() - 2];
+	double a = sequence[sequence.size() - 1];
+	
+	if (x == z && y == a) {
+		return true;
+	}
+	else
+		return false;
+}
+
+
+
+double alternatingAnswer(std::vector<double> sequence) {
+	double x = sequence[sequence.size() - 2];
+	//select the second to last value in the sequence
+	return x;
+}
